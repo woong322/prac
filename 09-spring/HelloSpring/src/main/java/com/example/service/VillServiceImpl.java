f119f1c2947bd985532efaa63fb6aaff334c679a
T Chester <Trevin.Chester@gmail.com>
2021-01-04 14:24:36 -0500

    week 8 materials

--- 09-spring/HelloSpring/src/main/java/com/example/service/VillServiceImpl.java
+++ 09-spring/HelloSpring/src/main/java/com/example/service/VillServiceImpl.java
@@ -6,12 +6,69 @@ import com.example.model.SuperVillain;
 import com.example.repository.VillDao;
 import com.example.repository.VillDaoImpl;
 
+/*
+ * EXPLANATION OF WHY THIS IS TIGHTLY COUPLED CODE:
+ * 
+ * Premise:
+ * 		we have a "ChimpDaoImpl" class extends MonkeyDao
+ * 		we have a "RhesusDaoImpl" class extends MonkeyDao <----this one is created later down the line
+ * 
+ * -----We'll need to have:
+ * 
+ * class MonkeyServiceImpl {
+ * 		//we want the Service class to be able to work with any Monkey
+ * 		MonkeyDao monkDao = new ChimpDaoImpl();
+ * }
+ * 
+ * class MonkeyServiceTWOImpl {
+ * 		//we want the Service class to be able to work with any Monkey
+ * 		MonkeyDao monkDao = new RhesusDaoImpl();
+ * }
+ * 
+ * 
+ * INSTEAD let's do this:
+ * 
+ * class MonkeyServiceImpl{
+ * 		//THIS version of MonkeyServiceImpl acts like a container for ANY type of MonkeyDao
+ * 
+ * 		MonkeyDao monkDao; //<--this is currently "null"
+ * 
+ * 
+ * 		public MonkeyServiceImpl(MonkeyDao monkDao){
+ * 			.......stuff......
+ * 		}
+ * 
+ * 		setMonkDao(MonkDao monkDao){
+ * 			........stuff.....
+ * 		}
+ * }
+ * 
+ * 
+ * 
+ * ^THIS is the idea of Dependency Injection (DI)
+ * >>>>DI is a type of design pattern
+ */
 public class VillServiceImpl implements VillService {
 
+	///////////STATIC MEMBERS
+	public static int howManyServicesCreated=0;
+	
+	///////////INITIALIZER BLOCK (non-static)
+	{
+		howManyServicesCreated++;
+	}
+	
+	
 	///////////STATE
-	private VillDao villDao = new VillDaoImpl();
 	
+	//WITHOUT Dependency Injection.
+	//the "new" keyword TIGHTLY COUPLES this class to a SPECIFIC object.
+	//you NEVER want to TIGHTLY COUPLE your DEPENDENCIES.
+	//private VillDao villDao = new VillDaoImpl();
 	
+	//WITH Dependency Injection
+	private VillDao villDao; //right now this value is "null"
+	private VillDao villDaoTwo;
 	
 	//////////BUSINESS LOGIC METHODS
 	@Override
@@ -21,10 +78,18 @@ public class VillServiceImpl implements VillService {
 	
 	//////////CONSTRUCTORS
 	public VillServiceImpl() {
+		System.out.println("In the service no arg constructor");
 	}
 
 	public VillServiceImpl(VillDao villDao) {
 		super();
+		System.out.println("In the service 1 arg constructor");
+		this.villDao = villDao;
+	}
+	
+	public VillServiceImpl(int i, VillDao villDao) {
+		super();
+		System.out.println("In the service 2 arg constructor: "+i);
 		this.villDao = villDao;
 	}
 	
@@ -34,7 +99,17 @@ public class VillServiceImpl implements VillService {
 	}
 
 	public void setVillDao(VillDao villDao) {
+		System.out.println("In the service setter ONE");
 		this.villDao = villDao;
+	}
+
+	public VillDao getVillDaoTwo() {
+		return villDaoTwo;
+	}
+
+	public void setVillDaoTwo(VillDao villDaoTwo) {
+		System.out.println("In the service setter TWO");
+		this.villDaoTwo = villDaoTwo;
 	}
 	
 
