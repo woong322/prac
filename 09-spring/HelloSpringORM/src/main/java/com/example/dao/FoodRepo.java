7233a72292047236d2d37a09482f749c84668a85
T Chester <Trevin.Chester@gmail.com>
2021-01-05 14:23:55 -0500

    week 8 materials

--- 09-spring/HelloSpringORM/src/main/java/com/example/dao/FoodRepo.java
+++ 09-spring/HelloSpringORM/src/main/java/com/example/dao/FoodRepo.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.model.Food;
 
 @Repository("foodRepo")
+@Transactional //this line modularizes the @Transactional annotation for the entire class
 public class FoodRepo { //aka FoodDao
 
 	//////FIELDS
@@ -29,44 +30,52 @@ public class FoodRepo { //aka FoodDao
 
 
 	/////METHODS
+	//@Transactional
 	public void insert(Food food) {
-		//basic session logic
-		Session ses = sesFact.openSession();
-		Transaction tx = ses.beginTransaction();
+//		//basic session logic
+//		Session ses = sesFact.openSession();
+//		Transaction tx = ses.beginTransaction();
+//		
+//		//business logic
+//		ses.save(food);
+//		
+//		//basic session logic
+//		tx.commit();
+//		ses.close();
 		
-		//business logic
-		ses.save(food);
-		
-		//basic session logic
-		tx.commit();
-		ses.close();
+		sesFact.getCurrentSession().save(food);
 	}
 	
+	//@Transactional
 	public Food selectById(int id) {
-		//basic session logic
-		Session ses = sesFact.openSession();
+//		//basic session logic
+//		Session ses = sesFact.openSession();
+//		
+//		//business logic
+//		Food food = ses.get(Food.class, id);
+//		
+//		//basic session logic
+//		ses.close();
+//		
+//		return food;
 		
-		//business logic
-		Food food = ses.get(Food.class, id);
-		
-		//basic session logic
-		ses.close();
-		
-		return food;
+		return sesFact.getCurrentSession().get(Food.class, id);
 	}
 	
-	
+	//@Transactional
 	public List<Food> selectAll() {
-		//basic session logic
-		Session ses = sesFact.openSession();
+//		//basic session logic
+//		Session ses = sesFact.openSession();
+//		
+//		//business logic
+//		List<Food> foodList = ses.createQuery("from Food", Food.class).list();
+//		
+//		//basic session logic
+//		ses.close();
+//		
+//		return foodList;
 		
-		//business logic
-		List<Food> foodList = ses.createQuery("from Food", Food.class).list();
-		
-		//basic session logic
-		ses.close();
-		
-		return foodList;
+		return sesFact.getCurrentSession().createQuery("from Food", Food.class).list();
 	}
 	
 }
