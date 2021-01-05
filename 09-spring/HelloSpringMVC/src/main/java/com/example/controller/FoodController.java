8d7304624e3d572ba0655a3a5f4550a0cf86a323
T Chester <Trevin.Chester@gmail.com>
2021-01-05 15:08:47 -0500

    week 8 materials

--- 09-spring/HelloSpringMVC/src/main/java/com/example/controller/FoodController.java
+++ 09-spring/HelloSpringMVC/src/main/java/com/example/controller/FoodController.java
@@ -1,9 +1,15 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.dao.FoodRepo;
+import com.example.model.Food;
 
 @Controller
 public class FoodController {
@@ -18,6 +24,15 @@ public class FoodController {
 	@Autowired
 	public FoodController(FoodRepo foodRepo) {
 		this.foodRepo = foodRepo;
+	}
+	
+	@RequestMapping(value="/getAllFood", method=RequestMethod.GET)
+	public @ResponseBody List<Food> getAllFoods(){
+		return foodRepo.selectAll();
+	}
+	
+	public Food getFoodById(int id) {
+		return foodRepo.selectById(id);
 	}
 	
 	
