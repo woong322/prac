81a92271ab2a7ab78e816d9198a93e2e16525411
T Chester <Trevin.Chester@gmail.com>
2021-01-06 17:41:26 -0500

    week 8 materials

--- 09-spring/HelloSpringBootAndSpringData/src/main/java/com/example/controller/FoodController.java
+++ 09-spring/HelloSpringBootAndSpringData/src/main/java/com/example/controller/FoodController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.Food;
@@ -15,9 +16,16 @@ import com.example.repo.FoodDao;
 public class FoodController {
 	
 	private FoodDao foodDao;
+	
+	@GetMapping("/food")
+	public Food findFood(@RequestParam("target") String target,
+			@RequestParam("targettwo") double targettwo) {
+		//return foodDao.findByDishName(target);
+		return foodDao.findByDishNameAndCalories(target,targettwo);
+	}
 
 	@PostMapping("/food")
-	public String findFood(@RequestBody Food food) {
+	public String insertFood(@RequestBody Food food) {
 		foodDao.save(food );
 		return "success";
 	}
--- 09-spring/HelloSpringBootAndSpringData/src/main/java/com/example/repo/FoodDao.java
+++ 09-spring/HelloSpringBootAndSpringData/src/main/java/com/example/repo/FoodDao.java
@@ -9,4 +9,21 @@ import com.example.model.Food;
 public interface FoodDao extends CrudRepository<Food,Integer> {
 
 	public List<Food> findAll();
+	public List<Food> findByOrderByDishName();
+	public List<Food> findByOrderByDishNameDesc();
+	
+	public Food findByDishName(String dishName);
+	public Food findbyCalories(double aifjaifj);
+	public Food findByDishNameAndCalories(String afiajij, double aiofjaijfj);
+	
+	/*
+	 * 
+	 * 
+	 * Look into @Query annotation
+	 * 
+	 * 
+	 * 
+	 * LOOK AT THE DOCUMENTATION FOR SPRING DATA HERE, IT HAS THE NAMING CONVENTIONS:
+	 * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/
+	 */
 }
