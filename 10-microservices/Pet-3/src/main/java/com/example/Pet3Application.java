6e3c406bece459f6ade11c8e89f72c5efb51d338
T Chester <Trevin.Chester@gmail.com>
2021-01-15 10:53:45 -0500

    week 9 materials

---
+++ 10-microservices/Pet-3/src/main/java/com/example/Pet3Application.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@EnableEurekaClient
+@EnableHystrix
+@SpringBootApplication
+public class Pet3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pet3Application.class, args);
+	}
+
+}
